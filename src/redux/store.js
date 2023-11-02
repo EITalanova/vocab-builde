0fedae17b14138d95a23ba73259d6c052855df1a
EITalanova <elenabelichenko77@gmail.com>
2023-11-02 18:06:09 +0200

    Start redux auth logic

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+
+const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whiteList: ['token'],
+};
+
+export const store = configureStore({
+    reducer: {
+       auth: persistReducer(),
+    },
+    middleware,
+    devTools: process.env.NODE_ENV === 'development',
+})
+
+export const persistor = persistStore(store);
