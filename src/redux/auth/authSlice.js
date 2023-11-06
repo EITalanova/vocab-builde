eaeac13528902840ee73b2c5944a7d31b22f6e21
EITalanova <elenabelichenko77@gmail.com>
2023-11-06 17:51:22 +0200

    Add authSlice logic

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,16 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { register } from "./authThunk";
 
 const initialState = {
-    user: { name: null, email: null },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
-}
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    extraReducers: {}
-})
+  name: "auth",
+  initialState,
+  extraReducers: (builder) =>
+    builder
+      .addCase(register.pending, (state, action) => state)
+      .addCase(register.fulfilled, (state, action) => state)
+      .addCase(register.rejected, (state, action) => state),
+});
 
 export const authReducer = authSlice.reducer;
---
+++ src/redux/auth/authThunk.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// axios.defaults.baseURL = '';
+
+const aetAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
+
+const clearAuthHeader = () => {
+    axios.defaults.headers.common.Authorization = '';
+}
+
+
+export const register = createAsyncThunk(
+    'auth/register',
+    async (credrntials, thunkAPI) => {
+        try {
+            const res = await axios.post('/users/singup', credrntials);
+            return res.data;
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message)
+        }
+    }
+)
+
+export const logIn = createAsyncThunk(
+    'auth/login',
+    async(credrntials, thunkAPI) => {}
+)
