ed1d6e4ad200e1662b97f55b551a4685308d2d38
EITalanova <elenabelichenko77@gmail.com>
2023-12-08 18:03:36 +0200

    Update authThunk.js

--- src/redux/auth/authThunk.js
+++ src/redux/auth/authThunk.js
@@ -1,30 +1,29 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = 'https://vocab-builder-backend.p.goit.global/api';
+axios.defaults.baseURL = "https://vocab-builder-backend.p.goit.global/api";
 
-const aetAuthHeader = token => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-}
+const aetAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
 
 const clearAuthHeader = () => {
-    axios.defaults.headers.common.Authorization = '';
-}
-
+  axios.defaults.headers.common.Authorization = "";
+};
 
 export const register = createAsyncThunk(
-    'auth/register',
-    async (credrntials, thunkAPI) => {
-        try {
-            const res = await axios.post('/users/signup', credrntials);
-            return res.data;
-        } catch (e) {
-            return thunkAPI.rejectWithValue(e.message)
-        }
+  "auth/register",
+  async (credrntials, thunkAPI) => {
+    try {
+      const res = await axios.post("/users/signup", credrntials);
+      return res.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
-)
+  }
+);
 
 export const logIn = createAsyncThunk(
-    'auth/login',
-    async(credrntials, thunkAPI) => {}
-)
+  "auth/login",
+  async (credrntials, thunkAPI) => {}
+);
