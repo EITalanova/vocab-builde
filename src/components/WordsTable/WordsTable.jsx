924b4c29fc1e2565e3f19ed4936d87a72b37fa77
EITalanova <elenabelichenko77@gmail.com>
2023-12-18 18:41:50 +0200

    Add components to dictionary page

--- src/components/WordsTable/WordsTable.jsx
+++ src/components/WordsTable/WordsTable.jsx
@@ -6,6 +6,7 @@ export const WordsTable = () => {
 
   return (
     
-<div>WordsTable</div>
+    <div>
+      WordsTable</div>
   );
 };
--- src/pages/Dictionary/Dictionary.jsx
+++ src/pages/Dictionary/Dictionary.jsx
@@ -1,10 +1,16 @@
+import { Dashboard } from "../../components/Dashboard/Dashboard";
+import { WordsPagination } from "../../components/WordsPagination/WordsPagination";
+import { WordsTable } from "../../components/WordsTable/WordsTable";
 
 import style from "./Dictionary.module.css";
 
-
 const Dictionary = () => {
   return (
-   <div>kkk</div>
+    <div>
+      <Dashboard />
+      <WordsTable />
+      <WordsPagination />
+    </div>
   );
 };
 
