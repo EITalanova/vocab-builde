5c1f9467d421be05ddbca7eab33796036910c367
EITalanova <elenabelichenko77@gmail.com>
2023-12-19 15:31:41 +0200

    Add close svg, modal and button components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+import style from "./Button.module.css";
+
+export const Button = () => {
+  return (
+    <div className={style.ButtonBox}>
+    Button
+    </div>
+  );
+};
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,12 @@
+import { ReactComponent as Close } from "../../assets/svg/close.svg";
+
+import style from "./Modal.module.css";
+
+export const Modal = ({ children }) => {
+  return (
+    <div className={style.modalBox}>
+      <Close />
+      {children}
+    </div>
+  );
+};
