ae923ae6f4e51ad73dca17b0b51b75b0920d6efc
EITalanova <elenabelichenko77@gmail.com>
2023-12-25 15:46:34 +0200

    Add buttons to dictionary

--- src/pages/Dictionary/Dictionary.jsx
+++ src/pages/Dictionary/Dictionary.jsx
@@ -1,8 +1,9 @@
 import { Dashboard } from "../../components/Dashboard/Dashboard";
 import { WordsPagination } from "../../components/WordsPagination/WordsPagination";
 import { WordsTable } from "../../components/WordsTable/WordsTable";
+import { Button } from "../../components/Button/Button";
 
-import style from "./Dictionary.module.css";
+// import style from "./Dictionary.module.css";
 
 const Dictionary = () => {
   return (
@@ -12,6 +13,8 @@ const Dictionary = () => {
       <Dashboard />
       <WordsTable />
       <WordsPagination />
+      <Button />
+      <Button />
     </div>
   );
 };
