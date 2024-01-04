ad3ba0c4db9f13932c104d264869e1c3c6bc9c8c
EITalanova <elenabelichenko77@gmail.com>
2024-01-04 21:32:10 +0200

    Add WordsTable structure

--- src/components/WordsTable/WordsTable.jsx
+++ src/components/WordsTable/WordsTable.jsx
@@ -1,12 +1,39 @@
-
+import { ReactComponent as Ua } from "../../assets/svg/tables/ua.svg";
+import { ReactComponent as Eng } from "../../assets/svg/tables/eng.svg";
 
 import style from "./WordsTable.module.css";
 
-export const WordsTable = () => {
+const table = [{ word: "Іван", translation: "Петров", category: 25 }];
 
+export const WordsTable = () => {
   return (
-    
     <div>
-      WordsTable</div>
+      <table>
+        <thead>
+          <tr>
+            Word
+            <Eng />
+          </tr>
+          <tr>
+            Translation
+            <Ua />
+          </tr>
+          <tr>Category</tr>
+          <tr>Progress</tr>
+          <tr></tr>
+        </thead>
+
+        <tbody>
+          {table.map((el) => {
+            <tr key={el.word + el.translation}>
+              <td>{el.word}</td>
+              <td>{el.translation}</td>
+              <td>{el.category}</td>
+            </tr>;
+          })}
+        </tbody>
+      </table>
+      WordsTable
+    </div>
   );
 };
